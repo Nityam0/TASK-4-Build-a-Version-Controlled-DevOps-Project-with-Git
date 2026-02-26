60843498fe80f7711b0eb68ba547cb0a1fae0ed8
Ubuntu <ubuntu@ip-172-31-34-162.ap-south-1.compute.internal>
2026-02-26 10:51:39 +0000

    hello

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
-print("Hello DevOps World!"))
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+logging.info("Application Started - Version 2.0")
+print("Hello DevOps World!")
+print("Login Feature Added")
+print("Version 2.0 Running Successfully")
